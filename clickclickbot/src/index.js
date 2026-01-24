4e6b79d7f05d5a2fca5b9801d1dadd290926e53b
Aziz <68770828+azizhakberd@users.noreply.github.com>
2026-01-24 14:38:07 +0500

    Unsafe prototype. Uses AI binding to access cloudflare AI api Models in use:     llama-3.1-8b-instruct No rate limiters No input size limit

--- clickclickbot/src/index.js
+++ clickclickbot/src/index.js
@@ -8,8 +8,25 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { Bot, Context, webhookCallback } from "grammy";
+
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env, ctx) {
+    const bot = new Bot(env.BOT_API_KEY, { botInfo: JSON.parse(env.BOT_INFO) });
+
+    bot.command("start", async (ctx) => {
+      await ctx.reply("Hello, world!");
+    });
+
+    bot.command("llm", async(ctx) => {
+      const message = ctx.match
+      const response = await env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
+        prompt: message
+      })
+
+      await ctx.reply(response.response)
+    })
+
+    return webhookCallback(bot, "cloudflare-mod")(request);
+  },
 };
