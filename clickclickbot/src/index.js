d9eeb4469614be56399e13b80cb7deb7dcb65409
Aziz <68770828+azizhakberd@users.noreply.github.com>
2026-01-24 20:47:18 +0500

    Reply messages now use MarkdownV2 formatting

--- clickclickbot/src/index.js
+++ clickclickbot/src/index.js
@@ -9,22 +9,45 @@
  */
 
 import { Bot, Context, webhookCallback } from "grammy";
+const telegramifyMarkdown = require("telegramify-markdown")
+import * as messageBank from "./messageBank";
+
+const MODEL_LLAMA_3_1_8b_instruct = "@cf/meta/llama-3.1-8b-instruct"
+let defaultLLMModel = MODEL_LLAMA_3_1_8b_instruct;
 
 export default {
   async fetch(request, env, ctx) {
     const bot = new Bot(env.BOT_API_KEY, { botInfo: JSON.parse(env.BOT_INFO) });
 
     bot.command("start", async (ctx) => {
-      await ctx.reply("Hello, world!");
+      await ctx.reply(messageBank.usageGuide);
     });
 
-    bot.command("llm", async(ctx) => {
+    bot.command("ask", async(ctx) => {
+      
       const message = ctx.match
-      const response = await env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
+
+    // generate reply message
+      
+      // input valid, inform user that message will be sent soon
+      await ctx.reply(messageBank.llmProcessStart, {
+        reply_parameters: {message_id: ctx.msg.message_id}
+      })
+
+      const response = await env.AI.run(defaultLLMModel, {
         prompt: message
       })
 
-      await ctx.reply(response.response)
+      // Issue: output of env.AI.run() is a markdown text, however it is not safe
+      // e.g. grammy requires certain symbols to be escaped
+
+      // Solution
+      let mdV2text = telegramifyMarkdown(response.response, "remove")
+
+      await ctx.reply(mdV2text, {
+          parse_mode: "MarkdownV2",
+          reply_parameters: {message_id: ctx.msg.message_id}
+        })
     })
 
     return webhookCallback(bot, "cloudflare-mod")(request);
---
+++ clickclickbot/src/messageBank.js
@@ -0,0 +1,5 @@
+export const usageGuide =
+`You can add me to your group conversation and give the permission to send messages to participate in conversation with you`
+
+export const llmProcessStart = 
+`Thinking`
